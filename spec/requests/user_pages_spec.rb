482b444139f1858b19907194dd0dea5a4e1608b0
nik <ngbaylo@gmail.com>
2013-12-20 14:27:10 +0200

    before/after registation state

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,7 +10,14 @@ describe "User pages" do
     it { should have_selector('title', text: 'Sign up') }
   end
 
+  describe "profile page" do
+    # Code to make a user variable
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
 
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
+  end
 
   describe "signup" do
 
@@ -38,8 +45,10 @@ describe "User pages" do
 
       describe "after saving the user" do
         before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
 
-        it { should visit root_path}
+        it { should have_selector('title', text: user.name)}
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
         it { should have_link('Sign out') }
 
         describe "followed by signout" do
